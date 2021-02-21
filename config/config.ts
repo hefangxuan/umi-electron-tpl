ffb0b6353a80def35ea41ac738609e6b4eadf894
hefangxuan <hefangxuan@qq.com>
2021-02-21 14:32:40 +0800

    - 增加配置信息

--- config/config.ts
+++ config/config.ts
@@ -3,6 +3,8 @@ import { appName, electronBuilder } from '../electronBuilader';
 import { routes } from './route';
 import path from 'path';
 
+const resolvePath = (dir: string) => path.join(__dirname, '../', dir);
+
 export default defineConfig({
   devtool: 'eval',
   electronBuilder,
@@ -21,8 +23,38 @@ export default defineConfig({
 
   copy: [
     {
-      from: 'loading/loading.html',
-      to: 'loading/loading.html',
+      from: 'files',
+      to: 'files',
     },
   ],
+  // 生成资源带 hash 尾缀
+  // 开发模式下 umi 会忽略此选项，不然热重载会出问题(很贴心)
+  hash: true,
+  // url 格式
+  history: {
+    type: 'hash',
+  },
+  // script、link 标签资源引入路径
+  publicPath: './',
+  // antd 主题配置
+  theme: {
+    '@primary-color': 'rgba(249, 163, 42, 1.000)',
+    '@link-color': 'rgba(249, 163, 42, 1.000)',
+    '@font-family': 'Arial, Helvetica, sans-serif',
+    '@line-height-base': '1.3',
+    '@border-radius-base': '6px',
+    '@font-size-base': '12px',
+  },
+  antd: {
+    // dark: true, // 开启暗色主题
+    compact: true, // 开启紧凑主题
+  },
+  // 路径别名
+  alias: {
+    '@': resolvePath('src'),
+    '@components': resolvePath('src/components'),
+    '@config': resolvePath('config'),
+    '@utils': resolvePath('src/utils'),
+    '@pages': resolvePath('src/pages'),
+  },
 });
