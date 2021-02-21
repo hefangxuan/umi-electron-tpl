edfca442ccdb9bc92c80b97a113dcafcd808e1ab
hefangxuan <hefangxuan@qq.com>
2021-02-21 21:00:31 +0800

    - 完成主页改造

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import styles from './index.less';
+import './index.less';
 
 export default function IndexPage() {
   return (
-    <div style={{ height: 10000 }}>
-      <h1 className={styles.title}>Pa 22a qei nd ex</h1>
+    <div className="home">
+      <div className="home-body">
+        <div id="box">
+          <p id="flashlight">
+            <span id="flash">UMI-ELECTRON-TPl</span>
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
