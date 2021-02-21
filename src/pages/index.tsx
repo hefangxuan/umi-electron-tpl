a7bd7893480fbaadf4a2b28981130524f96924d7
hefangxuan <hefangxuan@qq.com>
2021-02-21 23:12:16 +0800

    - 添加lottie动画组件

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import Lottie from 'react-lottie';
 import './index.less';
 
+import animationData from '@/assets/lf20_mdbdc5l7.json';
+
 export default function IndexPage() {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice',
+    },
+  };
   return (
     <div className="home">
       <div className="home-body">
+        <Lottie
+          options={defaultOptions}
+          height={300}
+          width={300}
+          isStopped={false}
+          isPaused={false}
+        />
         <div id="box">
           <p id="flashlight">
             <span id="flash">UMI-ELECTRON-TPl</span>
