eb1a002accbec2a0b9c11677e252aaf790b8a53a
hefangxuan <hefangxuan@qq.com>
2021-02-22 16:35:33 +0800

    - 将主进程抽离分多个文件

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,11 +1,17 @@
-import { app, BrowserWindow, protocol, screen } from 'electron';
-import createProtocol from '../../lib/umi-plugin-electron-builder/createProtocol';
+import { app, BrowserWindow } from 'electron';
 
 import installExtension, { REACT_DEVELOPER_TOOLS } from 'electron-devtools-installer';
 
 import isDevEnv from 'electron-is-dev';
-import { appName } from '../../electronBuilader';
-import * as path from 'path';
+import { createWindow } from './mainWindow';
+
+const gotTheLock = app.requestSingleInstanceLock();
+if (!gotTheLock) {
+  app.quit();
+}
+
+// 主窗口
+let mainWindow: BrowserWindow;
 
 // 引入启动窗口
 // const Elp = require('electron-launch-page');
@@ -16,106 +22,14 @@ if (isDevEnv) {
   dotenv.config();
 }
 
-// 主窗口
-let mainWindow: BrowserWindow;
-
-// loading窗口
-let motionWindow: BrowserWindow | null;
-
-// 自定义协议,主窗口
-protocol.registerSchemesAsPrivileged([
-  { scheme: 'app', privileges: { secure: true, standard: true } },
-]);
-
-// loading窗口
-function createMotionWindow() {
-  motionWindow = new BrowserWindow({
-    title: appName,
-    frame: false,
-    width: 470,
-    height: 280,
-    transparent: true,
-    webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false,
-    },
-    show: true,
-    movable: false, // motion window do not movable
-    skipTaskbar: true,
-    maximizable: false,
-    minimizable: false,
-    closable: false,
-  });
-
-  if (isDevEnv) {
-    motionWindow
-      .loadURL(`file://${path.join(process.cwd(), 'files/loading/loading.html')}`)
-      .catch((e) => console.log('加载文件错误: ', e));
-  } else {
-    motionWindow
-      .loadURL(`file://${path.join(__dirname, 'files/loading/loading.html')}`)
-      .catch((e) => console.log('加载文件错误: ', e));
+// 控制窗口数量
+app.on('second-instance', (event, commandLine, workingDirectory) => {
+  // 当运行第二个实例时,将会聚焦到 mainWindow 这个窗口
+  if (mainWindow) {
+    if (mainWindow.isMinimized()) mainWindow.restore();
+    mainWindow.focus();
   }
-}
-
-// 主窗口
-function createWindow() {
-  if (mainWindow) return;
-
-  const { width, height } = screen.getPrimaryDisplay().workAreaSize;
-  // 主窗口配置
-  const config = {
-    width: width * 0.9,
-    height: height * 0.8,
-    minWidth: 960,
-    minHeight: 720,
-    autoHideMenuBar: false,
-    title: appName,
-    backgroundColor: '#fff',
-    // transparent: !isMac,
-    // frame: isMac,
-    fullscreen: false,
-    webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false,
-      // webviewTag: true,
-      // enableRemoteModule: true,
-      // preload: path.join(process.cwd(), 'src/main/preload.js')
-    },
-    show: false,
-  };
-  mainWindow = new BrowserWindow(config);
-
-  // 隐藏默认菜单
-  mainWindow.setMenuBarVisibility(false);
-
-  // 创建loading窗口
-  createMotionWindow();
-
-  if (isDevEnv) {
-    mainWindow.loadURL(`http://localhost:${process.env.PORT}`).then(() => {
-      // if (!process.env.IS_TEST) mainWindow.webContents.openDevTools();
-    });
-  } else {
-    createProtocol('app');
-    mainWindow.loadURL('app://./index.html').then();
-  }
-
-  // 监听主窗口完成
-  mainWindow.on('ready-to-show', function () {
-    // mainWindow.setOpacity(0);
-    setTimeout(() => {
-      // mainWindow.setOpacity(1);
-      if (motionWindow) {
-        motionWindow.setOpacity(0);
-        motionWindow.destroy();
-        motionWindow = null;
-        mainWindow.show();
-        mainWindow.maximize();
-      }
-    }, 2000);
-  });
-}
+});
 
 // 监听软件运行
 app.on('ready', async () => {
@@ -123,7 +37,7 @@ app.on('ready', async () => {
     installExtension(REACT_DEVELOPER_TOOLS).then();
   }
   await app.whenReady();
-  createWindow();
+  mainWindow = createWindow();
 });
 
 // 监听软件关闭
@@ -134,8 +48,8 @@ app.on('window-all-closed', () => {
 });
 
 // 监听软件获取焦点
-app.on('activate', () => {
+app.on('activate', async () => {
   if (mainWindow === null) {
-    createWindow();
+    mainWindow = createWindow();
   }
 });
---
+++ src/main/mainWindow.ts
@@ -0,0 +1,73 @@
+// 主窗口
+import { BrowserWindow, protocol, screen } from 'electron';
+import { appName } from '../../electronBuilader';
+import isDevEnv from 'electron-is-dev';
+import createProtocol from '../../lib/umi-plugin-electron-builder/createProtocol';
+import { createMotionWindow } from './motionWindow';
+
+// 自定义协议,主窗口
+protocol.registerSchemesAsPrivileged([
+  { scheme: 'app', privileges: { secure: true, standard: true } },
+]);
+
+export function createWindow() {
+  // 主窗口
+  let mainWindow: BrowserWindow;
+
+  // loading窗口
+  let motionWindow: BrowserWindow | null;
+
+  const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+  // 主窗口配置
+  const config = {
+    width: width * 0.9,
+    height: height * 0.8,
+    minWidth: 960,
+    minHeight: 720,
+    autoHideMenuBar: false,
+    title: appName,
+    backgroundColor: '#fff',
+    // transparent: !isMac,
+    // frame: isMac,
+    fullscreen: false,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      // webviewTag: true,
+      // enableRemoteModule: true,
+      // preload: path.join(process.cwd(), 'src/main/preload.js')
+    },
+    show: false,
+  };
+  mainWindow = new BrowserWindow(config);
+
+  // 隐藏默认菜单
+  mainWindow.setMenuBarVisibility(false);
+
+  // 创建loading窗口
+  motionWindow = createMotionWindow();
+
+  if (isDevEnv) {
+    mainWindow.loadURL(`http://localhost:${process.env.PORT}`).then();
+  } else {
+    createProtocol('app');
+    mainWindow.loadURL('app://./index.html').then();
+  }
+
+  // 监听主窗口完成
+  mainWindow.on('ready-to-show', function () {
+    // mainWindow.setOpacity(0);
+    setTimeout(() => {
+      // mainWindow.setOpacity(1);
+      if (motionWindow) {
+        motionWindow.setOpacity(0);
+        motionWindow.destroy();
+        motionWindow = null;
+        mainWindow.show();
+        mainWindow.maximize();
+      }
+    }, 2000);
+  });
+
+  return mainWindow;
+}
